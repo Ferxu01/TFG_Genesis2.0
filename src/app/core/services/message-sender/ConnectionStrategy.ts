cd2e5c9eecece1c4359294927a206dd64b058ac7
Fernando Ortega <ferortjim@gmail.com>
2026-05-01 12:06:41 +0200

    chore: removed unused code and updated sensors examples data

--- src/app/core/services/message-sender/ConnectionStrategy.ts
+++
@@ -1,9 +0,0 @@
-import { ConnectionEntity } from 'app/models/Connection.model';
-import { Observable } from 'rxjs';
-
-export interface ConnectionStrategy {
-    sendMessage(
-        message: any,
-        connectionOptions: ConnectionEntity['options']
-    ): Observable<boolean>;
-}
